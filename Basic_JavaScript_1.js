7788c848a0f44b5450f553288c5bffc7e1375a81
SIAG0N <phillipziwadah@gmail.com>
2024-12-21 05:21:18 +0000

    added tags

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+<script>
+    function My_First_Function() {
+
+        var String = "Hug me, I'm Zimbabwean";
+        var result = String.fontcolor("green");
+        document.getElementById("Zimbabwean").innerHTML = result;
+            
+    }
+</script>
